1c65348f738cd6779a424e423179074fa20e20d6
Droid216 <bit216den@gmail.com>
2024-11-15 11:26:19 +0500

    update wb_advert

--- wb_sdk/response/nm_report_downloads_response.py
+++ wb_sdk/response/nm_report_downloads_response.py
@@ -7,6 +7,6 @@ from ..entities import Error
 class NmReportDownloadsResponse(BaseResponse):
     """Создать отчёт по воронке продаж."""
     data: str
-    error: bool
-    errorText: str
+    error: Optional[bool] = None
+    errorText: Optional[str] = None
     additionalErrors: Optional[list[Error]] = []
