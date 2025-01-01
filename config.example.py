95fe098d3eabc5c36c51872410fdc9b0305b90e8
Droid216 <bit216den@gmail.com>
2025-01-01 13:46:16 +0500

    delete config.example.py

--- config.example.py
+++
@@ -1,7 +0,0 @@
-DB_USER = "postgres"
-DB_PASS = "your_password"
-DB_HOST = "your_host"
-DB_NAME = "your_database"
-DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
-
-PROXY = "http://user:password@host:port"
