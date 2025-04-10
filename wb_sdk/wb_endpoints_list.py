7d721ed15a3a259b495f620d34dd80fbb1b5422b
Droid216 <bit216den@gmail.com>
2025-04-10 10:59:23 +0500

    .wildberries.ru

--- wb_sdk/wb_endpoints_list.py
+++ wb_sdk/wb_endpoints_list.py
@@ -10,10 +10,10 @@ class WBAPIFactory:
 
     api_list: dict[Type[BaseResponse], str] = {
         SupplierSalesResponse: 'https://statistics-api.wildberries.ru/api/v1/supplier/sales',
-        PromotionAdvertsResponse: 'https://advert-api..wildberries.ru/adv/v1/promotion/adverts',
-        FullstatsResponse: 'https://advert-api..wildberries.ru/adv/v2/fullstats',
+        PromotionAdvertsResponse: 'https://advert-api.wildberries.ru/adv/v1/promotion/adverts',
+        FullstatsResponse: 'https://advert-api.wildberries.ru/adv/v2/fullstats',
         NMReportDetailResponse: 'https://seller-analytics-api.wildberries.ru/api/v2/nm-report/detail',
-        ListGoodsFilterResponse: 'https://discounts-prices-api..wildberries.ru/api/v2/list/goods/filter',
+        ListGoodsFilterResponse: 'https://discounts-prices-api.wildberries.ru/api/v2/list/goods/filter',
         SupplierReportDetailByPeriodResponse: 'https://statistics-api.wildberries.ru/api/v5/supplier/reportDetailByPeriod',
         PaidStorageResponse: 'https://seller-analytics-api.wildberries.ru/api/v1/paid_storage',
         PaidStorageStatusResponse: 'https://seller-analytics-api.wildberries.ru/api/v1/paid_storage/tasks/{task_id}/status',
