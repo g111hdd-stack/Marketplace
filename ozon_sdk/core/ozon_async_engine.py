e3a02d678d4c5aeec0c4abc569d15e980a74bcc4
Droid216 <bit216den@gmail.com>
2025-06-16 11:00:55 +0500

    update ozon_async_engine.py

--- ozon_sdk/core/ozon_async_engine.py
+++ ozon_sdk/core/ozon_async_engine.py
@@ -43,7 +43,7 @@ class OzonAsyncEngine:
                         if response.status != 200:
                             logger.info(f"Получен ответ от {url} ({response.status})")
                             logger.error(f"Попытка повторного запроса. Осталось попыток: {retry - 1}")
-                            await asyncio.sleep(60)
+                            await asyncio.sleep(120)
                             retry -= 1
                             continue
                         return await response.json(content_type=None)
@@ -69,7 +69,7 @@ class OzonAsyncEngine:
                         if response.status != 200:
                             logger.info(f"Получен ответ от {url} ({response.status})")
                             logger.error(f"Попытка повторного запроса. Осталось попыток: {retry - 1}")
-                            await asyncio.sleep(60)
+                            await asyncio.sleep(120)
                             retry -= 1
                             continue
                         return await response.json()
