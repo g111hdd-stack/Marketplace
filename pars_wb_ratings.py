cb7791ad011684e74e6a25dc9988a389ef11cfed
Droid216 <bit216den@gmail.com>
2025-10-28 17:04:58 +0700

    update pars_wb_ratings.py

--- pars_wb_ratings.py
+++ pars_wb_ratings.py
@@ -43,50 +43,52 @@ async def pars_query(session: ClientSession, query: DataQuery, db_conn: AsyncDbC
             "resultset": "catalog",
             "sort": "popular"
         }
+        try:
+            async with session.get(URL, params=params, headers=HEADERS) as response:
+                if response.content_type != 'application/json':
+                    text = await response.text()
+                    try:
+                        data = json.loads(text)
+                    except json.JSONDecodeError:
+                        logger.error(f"{product}: {query_text} - Ошибка парсинга JSON")
+                        return
+                else:
+                    data = await response.json()
 
-        async with session.get(URL, params=params, headers=HEADERS) as response:
-            if response.content_type != 'application/json':
-                text = await response.text()
-                try:
-                    data = json.loads(text)
-                except json.JSONDecodeError:
-                    logger.error(f"{product}: {query_text} - Ошибка парсинга JSON")
-                    return
-            else:
-                data = await response.json()
+                cards = data.get("data", {}).get("products", [])
 
-            cards = data.get("data", {}).get("products", [])
+                if not cards:
+                    logger.error(f"{product}: {query_text} - Данные отсутствуют")
 
-            if not cards:
-                logger.error(f"{product}: {query_text} - Данные отсутствуют")
+                target_card = next((card for card in cards if str(card.get("id")) == sku), None)
+                if not target_card:
+                    continue
 
-            target_card = next((card for card in cards if str(card.get("id")) == sku), None)
-            if not target_card:
-                continue
+                stat = target_card.get("log")
+                if not isinstance(stat, dict):
+                    logger.error(f"{product}: {query_text} - Карточка найдена, но нет данных по промо")
+                    continue
 
-            stat = target_card.get("log")
-            if not isinstance(stat, dict):
-                logger.error(f"{product}: {query_text} - Карточка найдена, но нет данных по промо")
-                continue
+                cpm = stat.get("cpm")
+                promo_position = stat.get("promoPosition")
+                position = stat.get("position")
+                advert_id = stat.get("advertId")
 
-            cpm = stat.get("cpm")
-            promo_position = stat.get("promoPosition")
-            position = stat.get("position")
-            advert_id = stat.get("advertId")
+                if all([m is None for m in [cpm, promo_position, position, advert_id]]):
+                    logger.error(f"{product}: {query_text} - Карточка найдена, но нет данных по промо в log")
+                    continue
 
-            if all([m is None for m in [cpm, promo_position, position, advert_id]]):
-                logger.error(f"{product}: {query_text} - Карточка найдена, но нет данных по промо в log")
-                continue
-
-            await db_conn.add_rating(
-                DataRating(query_id=query.id_query,
-                           cpm=float(cpm) if cpm is not None else None,
-                           promo_position=int(promo_position) if promo_position is not None else None,
-                           position=int(position) if position is not None else None,
-                           advert_id=str(advert_id) if advert_id is not None else None))
-            info = f"Позиция в промо: {promo_position} | Позиция: {position} | CPM: {cpm} | РК: {advert_id}"
-            logger.info(f"{product}: {query_text} - {info}")
-            return
+                await db_conn.add_rating(
+                    DataRating(query_id=query.id_query,
+                               cpm=float(cpm) if cpm is not None else None,
+                               promo_position=int(promo_position) if promo_position is not None else None,
+                               position=int(position) if position is not None else None,
+                               advert_id=str(advert_id) if advert_id is not None else None))
+                info = f"Позиция в промо: {promo_position} | Позиция: {position} | CPM: {cpm} | РК: {advert_id}"
+                logger.info(f"{product}: {query_text} - {info}")
+                return
+        except Exception as e:
+            logger.error(f"Ошибка при запросе {URL} для {query_text}: {e}")
     else:
         logger.info(f"Товар не найден на первых {MAX_PAGE} страницах")
         await db_conn.add_rating(DataRating(query_id=query.id_query,
